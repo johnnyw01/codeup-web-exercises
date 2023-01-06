8d62d9525eb7b77c2558fff36057e51d5dc20346
Johnny Wells <johnnyw01github@gmail.com>
2023-01-06 09:40:44 -0600

    re-pushing previously pushed exercises.

--- js/bonus_questions.js
+++ js/bonus_questions.js
@@ -294,43 +294,136 @@
 // ================================= WARM UP
 //
 // Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
+//
+//     const pets = [
+//     {
+//         name: 'Sparky',
+//         type: 'Fish',
+//         age: 4
+//     },
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//     {
+//         name: 'Bubba',
+//         type: 'Dog',
+//         age: 5
+//     },
+//     {
+//         name: 'Beans',
+//         type: 'Dog',
+//         age: 3
+//     },
+//         {
+//             name: 'Mr. Salmon',
+//             type: 'Fish',
+//             age: 1
+//         }
+// ];
+//
+// let longestName = "" ;
+// const returnLongestPetName = (pets) => {
+//     pets.forEach((pet) => {
+//         if(longestName.length < pet.name.length){
+//             longestName = pet.name;
+//         }
+//     });
+//     return longestName;
+// }
+//
+// console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
 
-    const pets = [
-    {
-        name: 'Sparky',
-        type: 'Fish',
-        age: 4
-    },
-    {
-        name: 'Mr. Pig',
-        type: 'Cat',
-        age: 4
-    },
-    {
-        name: 'Bubba',
-        type: 'Dog',
-        age: 5
-    },
-    {
-        name: 'Beans',
-        type: 'Dog',
-        age: 3
-    },
-        {
-            name: 'Mr. Salmon',
-            type: 'Fish',
-            age: 1
-        }
-];
+// ================================= WARM UP
+// Write a function, returnLastTwoChars that returns the last two characters of an input string. Assume a string input of at least two characters.
+// returnLastTwoChars(‘cat’) // returns ‘at’
+// returnLastTwoChars(‘hello’) // returns ‘lo’
+// returnLastTwoChars(‘hi’) // returns ‘hi’
 
-let longestName = "" ;
-const returnLongestPetName = (pets) => {
-    pets.forEach((pet) => {
-        if(longestName.length < pet.name.length){
-            longestName = pet.name;
-        }
-    });
-    return longestName;
-}
 
-console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
+// const returnLastTwoChars= (input)=>{
+//     if(input.length >= 2){
+//         return input.slice(-2);
+//     }else{
+//         return 'Input needs to be at least 2 characters long.'
+//     }
+// }
+//
+// console.log(returnLastTwoChars('cat'));
+// console.log(returnLastTwoChars('hello'));
+// console.log(returnLastTwoChars('hi'));
+
+
+
+// EXTRA CHALLENGE
+// Create a function, returnLastCharsReversed that takes in two arguments:
+// 1. a string of characters
+// 2. an integer that represents how many characters should be returned
+
+// The function should return a string of characters, equal to the number specified
+// by the second argument, in reverse order. If 0 is passed as a second argument,
+// return an empty string. Assume only valid inputs and that the second argument
+// will NOT exceed the length of the input string.
+
+
+// const returnLastCharsReversed = (input1, input2) =>{
+//     if(typeof input1 !== 'string' && typeof input2 !== 'number'){
+//         return false;
+//     }else {
+//         let reversedInput = input1.split("").reverse().slice(0, input2).join("");
+//         return reversedInput
+//
+//     }
+// }
+//
+// console.log(returnLastCharsReversed('cat', 0));
+// console.log(returnLastCharsReversed('cat', 1));
+// console.log(returnLastCharsReversed('cat', 2));
+// console.log(returnLastCharsReversed('cat', 3));
+
+
+//     Examples:
+// returnLastCharsReversed(‘cat’, 0) // returns ‘’
+// returnLastCharsReversed(‘cat’, 1) // returns ‘t’
+// returnLastCharsReversed(‘cat’, 2) // returns ‘ta’
+// returnLastCharsReversed(‘cat’, 3) // returns ‘tac’
+// returnLastCharsReversed(‘codeup’, 1) // returns ‘p’
+// returnLastCharsReversed(‘codeup’, 3) // returns ‘pue’
+// returnLastCharsReversed(‘codeup’, 4) // returns ‘pued’
+
+// Write a function, iBeforeE that takes in a string and returns the string with any ‘ei’ characters replaced with ‘ie’.
+
+// const iBeforeE = (str) => {
+//     if(str.includes('ei')){
+//         let newStr = str.replaceAll('ei','ie');
+//         return newStr;
+//     }
+// }
+//
+// console.log(iBeforeE('ei'));
+// console.log(iBeforeE('height'));
+// console.log(iBeforeE('heist'));
+// console.log(iBeforeE('their'));
+// console.log(iBeforeE('theirtheir'));
+
+
+// iBeforeE(‘ei’) // returns ‘ie’
+// iBeforeE(‘height’) // returns ‘hieght’
+// iBeforeE(‘heist’) // returns ‘hiest’
+// iBeforeE(‘their’) // returns ‘thier’
+// iBeforeE(‘theirtheir’) // returns ‘thierthier’
+
+// ================================= WARM UP
+// Create a function, filterList, that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// const filterList = (lst)=>{
+//    return lst.filter((num) => typeof num !== 'string');
+// }
+// console.log(filterList([1,2,'a','b']));
+// console.log(filterList([1,'a','b', 0, 15]));
+// console.log(filterList([1,2,'aasf','1','123',123]));
+
+// filterList([1,2,‘a’,‘b’]) // returns [1,2]
+// filterList([1,‘a’,‘b’,0,15]) // returns [1,0,15]
+// filterList([1,2,‘aasf’,‘1’,‘123’,123]) // returns [1,2,123]
