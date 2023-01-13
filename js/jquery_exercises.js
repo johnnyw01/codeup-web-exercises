b2a9445e2d125091c0c8634eef8332fc4e70976d
Johnny Wells <johnnyw01github@gmail.com>
2023-01-13 14:44:49 -0600

    completed the Id Selector section of the jquery selector exercises.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,20 @@
 $(() =>{
     "use strict";
 
-    $(() => {
-        alert( 'The DOM has DOMING!' );
-    });
+    // $(() => {
+    //     alert( 'The DOM has DOMING!' );
+    // });
+
+
+    //Id Selectors
+    let getH1 = $('#1').html();
+    // alert(getH1);
+    let getDiv = $('#2').html();
+    // alert(getDiv);
+
+    //Class Selectors
+
+
+
 
 })
