c167bb3a9131b736d57d51e3658ca1f1df001532
Johnny Wells <johnnyw01github@gmail.com>
2023-01-13 15:06:30 -0600

    completed the final section of the jquery selector exercise.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -7,12 +7,24 @@ $(() =>{
 
 
     //Id Selectors
-    let getH1 = $('#1').html();
+    // let getH1 = $('#1').html();
     // alert(getH1);
-    let getDiv = $('#2').html();
+    // let getDiv = $('#2').html();
     // alert(getDiv);
 
     //Class Selectors
+
+  // $('.codeup').css('border', '1px solid red');
+
+    //Element Selectors
+
+    $('li').css('font-size', '20px');
+    $('h1').css('background-color','yellow');
+    $('li').css('background-color','yellow');
+    $('p').css('background-color','yellow');
+
+    $('h1, li, p').css('background-color','red');
+
 
 
 
